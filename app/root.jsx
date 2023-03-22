eb8833663673d5542209b1149b7557bc492f1d12
IgnaDev <arcortoon@gmail.com>
2023-03-22 18:59:58 -0300

    lear: aplique normalize y google font con link

--- app/root.jsx
+++ app/root.jsx
@@ -15,13 +15,34 @@ export function meta(){
 }
 
 export function links(){
-    return(
+    return[
+        {
+            rel: 'stylesheet',
+            href: 'https://necolas.github.io/normalize.css/8.0.1/normalize.css'
+        },
         {
             rel: 'stylesheet',
             href: styles
+        },
+        {
+            rel: 'preconnect',
+            href: "https://fonts.googleapis.com"
+        },
+        {
+            rel: 'preconnect',
+            href: "https://fonts.gstatic.com" ,
+            crossOrigin: true
+        },
+        {
+            rel: 'stylesheet',
+            href: "https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap"
         }
-    )
+    ]
 }
+
+
+
+
 
 export default function App(){
     return(
