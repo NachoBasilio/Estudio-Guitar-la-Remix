033eda04d8ca28503688f446244a9f1c819c0a25
IgnaDev <arcortoon@gmail.com>
2023-03-24 20:24:44 -0300

    start: comenzando con los estilos de las diferentes secciones

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,6 +1,35 @@
+import imagen from '../../public/img/nosotros.jpg'
+import styles from "../styles/nosotros.css"
+
+export function meta() {
+  return(
+    {
+        title: 'GuitarLA - Sobre Nosotros',
+    }
+)
+}
+
+export function links(){
+  return[
+    {
+      rel: "stylesheet",
+      href: styles
+    }
+  ]
+}
+
 function Nosotros() {
   return (
-    <div>nosotros</div>
+    <main className="contenedor nosotros">
+      <h2 className="heading">Nosotros</h2>
+      <div className="contenido">
+        <img src={imagen} alt="imagen sobre nosotros" />
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus adipisci autem quod nisi, possimus magnam saepe tenetur. Itaque distinctio quia pariatur dolorem aliquam repellendus, numquam vitae amet quidem recusandae, soluta corrupti dolor consequuntur! Voluptas obcaecati aspernatur accusamus, facere quasi et dolor provident adipisci quas reiciendis id ipsum earum deleniti qui optio. Assumenda, inventore quae?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque doloribus obcaecati vero voluptate, possimus, inventore vel earum exercitationem dolorum magni atque officiis id eos. Necessitatibus eius mollitia cumque neque nulla, fugit cum. Maiores iusto itaque perspiciatis enim repellat quibusdam esse debitis vero similique ex repudiandae numquam, harum quas, eveniet consequuntur necessitatibus aliquid! Quas, nulla.</p>
+        </div>
+      </div>
+    </main>
   )
 }
 
