638d502d6e1a9c2d4e7e1f438aca333b5e7a3533
IgnaDev <arcortoon@gmail.com>
2023-04-10 22:18:05 -0300

    feat: carga de datos a cada una de las guitarras y estilos

--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -1,7 +1,15 @@
 import { useLoaderData } from "@remix-run/react"
 import { getGuitarra } from "~/models/guitarras.server"
+import styles from '~/styles/guitarras.css'
 
-
+export function links(){
+    return [
+      {
+        rel: 'stylesheet',
+        href: styles
+      },
+    ]
+  }
 
 export async function loader({request, params}){
     const {guitarraUrl} = params
@@ -14,8 +22,17 @@ export async function loader({request, params}){
 
 export default function Guitarra() {
     const guitarra = useLoaderData()
-    console.log(guitarra)
+    const {nombre, descripcion, imagen, precio} = guitarra.data[0].attributes
     return (
-        <div>$guitarraUrl</div>
+        <main className="contenedor guitarra">
+            <img className="imagen" src={imagen.data.attributes.url} alt={"imagen de la guitarra "+nombre} />
+
+            <div className="contenido">
+                <h3>{nombre}</h3>
+                <p className="texto">{descripcion}</p>
+                <p className="precio">${precio}</p>
+            </div>
+        </main>
+        
     )
 }
