b95e41d05b68c57e48739c95a216313f0d1f4964
IgnaDev <arcortoon@gmail.com>
2023-04-13 21:07:29 -0300

    feat aplicando todo lo necesario para blogs

--- app/components/post.jsx
+++ app/components/post.jsx
@@ -3,17 +3,16 @@ import { Link } from "@remix-run/react"
 
 
 export default function Post({post}) {
-    console.log(post)
     const {contenido, imagen, titulo, url, publishedAt} = post
   return (
-    <article>
-        <img src={imagen.data.attributes.formats.small.url} className="imagen" alt={`imagen blog ${titulo}`} />
-        <div className="contenido">
-            <h3>{titulo}</h3>
-            <p className="fecha">{publishedAt}</p>
-            <p className="resumen">{contenido}</p>
-            <Link to={`/posts/${url}`}>Leer post</Link>
-        </div>
+    <article className="post">
+    <img className="imagen" src={imagen.data.attributes.formats.small.url} alt={`imagen blog ${titulo}`} />
+    <div className="contenido">
+        <h3>{titulo}</h3>
+        <p className='fecha'>{publishedAt}</p>
+        <p className="resumen">{contenido}</p>
+        <Link className='enlace' to={`/blog/${url}`}>Leer Post</Link>
+    </div>
     </article>
   )
 }
--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,10 +1,21 @@
 import { useLoaderData } from "@remix-run/react"
 import Post from "~/components/post"
 import { getPosts } from "~/models/post.server"
+import styles from "~/styles/blog.css"
+
 
 export async function loader(){
   const posts = await getPosts()
   return posts.data
+}
+
+export function links(){
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    },
+  ]
 }
 
 function Blog() {
