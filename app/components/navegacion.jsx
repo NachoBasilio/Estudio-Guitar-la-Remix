cd3679c9707289a405841abd0afd048fa84aded2
IgnaDev <arcortoon@gmail.com>
2023-04-19 18:27:28 -0300

    feat: aplicando nested routes, esta genial

--- app/components/navegacion.jsx
+++ app/components/navegacion.jsx
@@ -14,8 +14,8 @@ function Navegacion() {
     className={location.pathname === "/nosotros" ? "active" : ""}
     >Nosotros</Link>
     <Link 
-    to="/tienda"
-    className={location.pathname === "/tienda" ? "active" : ""}
+    to="/guitarras"
+    className={location.pathname === "/guitarras" ? "active" : ""}
     >Tienda</Link>
     <Link 
     to="/blog"
--- app/routes/guitarras/index.jsx
+++ app/routes/guitarras/index.jsx
@@ -1,16 +1,7 @@
 import { useLoaderData } from "@remix-run/react"
 import ListadoGuitarras from "~/components/listado-guitarras"
 import { getGuitarras } from "~/models/guitarras.server"
-import styles from "~/styles/guitarras.css"
 
-export function links(){
-  return [
-    {
-      rel: 'stylesheet',
-      href: styles
-    },
-  ]
-}
 
 export async function loader(){
   const guitarras = await getGuitarras()
