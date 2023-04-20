7e86c7b2af276d0c11034e29987402001939d2b0
IgnaDev <arcortoon@gmail.com>
2023-04-20 18:51:26 -0300

    fear: estructura y estilos a carrito

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import styles from '~/styles/carrito.css'
+
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    },
+  ]
+}
+
+export function meta() {
+    return{ 
+        title: 'GuitarLA - Carrito de compras',
+        description: 'GuitarLA, carrito de compras'
+    }
+}
 
 function Carrito() {
   return (
-    <div>carrito</div>
+    <main className='contenedor'>
+        <h1 className='heading'>Carrito de compras</h1>
+        <div className='contenido'>
+            <div className='carrito'>
+                <h2 className>Articulos</h2>
+            </div>
+            <aside className='resumen'>
+                <h3>Resumen del pedido</h3>
+                <p>Total a pagar: $</p>
+            </aside>
+
+        </div>
+    </main>
   )
 }
 
