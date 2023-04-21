fb381959323d71e7dd901d7516239871a1cf1c1d
IgnaDev <arcortoon@gmail.com>
2023-04-21 17:35:17 -0300

    feat: boton, formulario y estilos

--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -54,6 +54,23 @@ export default function Guitarra() {
                 <h3>{nombre}</h3>
                 <p className="texto">{descripcion}</p>
                 <p className="precio">${precio}</p>
+
+                <form className="formulario">
+                    <label htmlFor="cantidad">Cantidad</label>
+                    <select name="" id="cantidad">
+                        <option value="">-- Seleccione --</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+
+                    <input 
+                    type="submit"
+                    value="Agregar al carrito"                     
+                    />
+                </form>
             </div>
         </main>
         
