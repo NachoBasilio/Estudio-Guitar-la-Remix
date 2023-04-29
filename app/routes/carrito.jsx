917ef761d2b298539dcc6d274f734840ba8a13e2
IgnaDev <arcortoon@gmail.com>
2023-04-29 20:32:00 -0300

    feat: estilos agregados

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -26,7 +26,7 @@ function Carrito() {
         <div className='contenido'>
             <div className='carrito'>
               <h2 className>Articulos</h2>
-              {carrito.length === 0 ? 'carrito Vacio': (
+              {carrito.length === 0 ? 'Carrito vacio': (
                 carrito.map(producto=>(
                   <div key={producto.key} className='producto'>
                     <div>
@@ -34,6 +34,7 @@ function Carrito() {
                     </div>
                     <div>
                       <p className="nombre">{producto.nombre}</p>
+                      <p className=''> Cantidad {producto.cantidad}</p>
                       <p className='precio'>$ <span>{producto.precio}</span></p>
                       <p className='subtotal'>Subtotal: $ <span>{producto.precio * producto.cantidad}</span></p>
                     </div>
